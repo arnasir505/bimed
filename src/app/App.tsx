4ccff0857a40690e0aedcf4d6ec0be19178a0594
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-10-04 15:17:28 +0600

    Uninstall styled-components, create FSD file structure

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import HeaderTop from '../widgets/header/headerTop/HeaderTop';
+
+function App() {
+  return (
+    <>
+      <HeaderTop />
+    </>
+  );
+}
+
+export default App;
---
+++ src/widgets/header/headerTop/HeaderTop.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@shared/ui';
+import { Typography } from 'antd';
+
+const HeaderTop = () => {
+  return (
+    <Container>
+      <Typography.Title>Hello</Typography.Title>
+    </Container>
+  );
+};
+
+export default HeaderTop;
