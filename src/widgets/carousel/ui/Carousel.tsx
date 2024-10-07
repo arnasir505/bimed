80b40fd128c624024eb1689c70053bebcb4e18d9
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-10-07 15:42:04 +0600

    Create main carousel

--- src/widgets/carousel/ui/Carousel.tsx
+++ src/widgets/carousel/ui/Carousel.tsx
@@ -1,17 +1,22 @@
-import { Carousel as AntCarousel } from 'antd';
+import { Carousel as AntCarousel, Flex, Image, Typography } from 'antd';
+import './style.css';
 
 export const Carousel = () => {
   return (
-    <AntCarousel className='carousel'>
-      <div>
-        <h3>1</h3>
-      </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
+    <AntCarousel className='carousel' draggable autoplay>
+      {[...Array(6)].map((_e, i) => (
+        <div className='carousel-item' key={i}>
+          <Flex vertical className='carousel-item__text' align='start' justify='space-between'>
+            <Image src='src/assets/images/logo-white.svg' preview={false} height={66} />
+            <Typography.Title className='carousel-item__title'>
+              Lorem ipsum dolor sit amet, consectetur
+            </Typography.Title>
+            <Typography.Text className='carousel-item__subtitle'>
+              Lorem ipsum dolor sit amet, consectetur
+            </Typography.Text>
+          </Flex>
+        </div>
+      ))}
     </AntCarousel>
   );
 };
