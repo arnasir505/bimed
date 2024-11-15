f5449535db9596e9ed96d9aef531d1bbf925476f
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-11-15 13:56:04 +0600

    Add show all button to order history

--- src/widgets/profile-order-history/ui/ProfileOrderHistory.tsx
+++ src/widgets/profile-order-history/ui/ProfileOrderHistory.tsx
@@ -5,11 +5,22 @@ import { selectOrderHistory } from 'entities/user';
 import { Button, Flex, Typography } from 'antd';
 import dayjs from 'dayjs';
 import productNoImage from 'assets/images/product-no-image.png';
-import { ArrowUpOutlined } from '@ant-design/icons';
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 
 export const ProfileOrderHistory = () => {
   const orderHistory = useAppSelector(selectOrderHistory);
   if (!orderHistory) return;
+
+  const [itemsToShow, setItemsToShow] = useState<string[]>([]);
+
+  const onShowAllClick = (id: string) => {
+    if (itemsToShow.includes(id)) {
+      setItemsToShow((prev) => prev.filter((itemId) => itemId !== id));
+    } else {
+      setItemsToShow((prev) => [...prev, id]);
+    }
+  };
 
   return orderHistory.length > 0 ? (
     <Flex vertical className='order-history-wrap'>
@@ -41,44 +52,50 @@ export const ProfileOrderHistory = () => {
               <Typography.Text className='order-history__date'>{orderDate}</Typography.Text>
             </Flex>
             <Flex className='order-history__items'>
-              <Flex vertical gap={'12px'}>
-                {order.items.map(({ product, quantity }) => (
-                  <Flex key={product.id} className='cart-item'>
-                    <div className='cart-item__img-wrap order-history-item__img-wrap'>
-                      <img src={product.img ? `/${product.img}` : productNoImage} alt='' />
-                    </div>
-                    <Flex vertical className='cart-item__content order-history-item__content'>
-                      <Typography.Text className='cart-item__name'>{product.name}</Typography.Text>
-                      <Flex vertical gap={'8px'}>
-                        <Flex className='cart-total__info'>
-                          <Typography.Text className='cart-total__info-text'>Цена:</Typography.Text>
-                          {product.newPrice ? (
-                            <Flex gap={'10px'}>
-                              <Typography.Text className='product-card__oldPrice'>
+              <Flex vertical className='order-history__cart-items'>
+                {order.items
+                  .slice(0, itemsToShow.includes(order.id) ? undefined : 1)
+                  .map(({ product, quantity }) => (
+                    <Flex key={product.id} className='cart-item'>
+                      <div className='cart-item__img-wrap order-history-item__img-wrap'>
+                        <img src={product.img ? `/${product.img}` : productNoImage} alt='' />
+                      </div>
+                      <Flex vertical className='cart-item__content order-history-item__content'>
+                        <Typography.Text className='cart-item__name'>
+                          {product.name}
+                        </Typography.Text>
+                        <Flex vertical gap={'8px'}>
+                          <Flex className='cart-total__info'>
+                            <Typography.Text className='cart-total__info-text'>
+                              Цена:
+                            </Typography.Text>
+                            {product.newPrice ? (
+                              <Flex gap={'10px'}>
+                                <Typography.Text className='product-card__oldPrice'>
+                                  {product.oldPrice} <span>c</span>
+                                </Typography.Text>
+                                <Typography.Text className='cart-item__price order-history-item__price'>
+                                  {product.newPrice} <span>c</span>
+                                </Typography.Text>
+                              </Flex>
+                            ) : (
+                              <Typography.Text className='cart-item__price order-history-item__price'>
                                 {product.oldPrice} <span>c</span>
                               </Typography.Text>
-                              <Typography.Text className='cart-item__price order-history-item__price'>
-                                {product.newPrice} <span>c</span>
-                              </Typography.Text>
-                            </Flex>
-                          ) : (
-                            <Typography.Text className='cart-item__price order-history-item__price'>
-                              {product.oldPrice} <span>c</span>
+                            )}
+                          </Flex>
+                          <Flex className='cart-total__info'>
+                            <Typography.Text className='cart-total__info-text'>
+                              Количество:
                             </Typography.Text>
-                          )}
-                        </Flex>
-                        <Flex className='cart-total__info'>
-                          <Typography.Text className='cart-total__info-text'>
-                            Количество:
-                          </Typography.Text>
-                          <Typography.Text className='order-history-item__quantity'>
-                            {quantity}
-                          </Typography.Text>
+                            <Typography.Text className='order-history-item__quantity'>
+                              {quantity}
+                            </Typography.Text>
+                          </Flex>
                         </Flex>
                       </Flex>
                     </Flex>
-                  </Flex>
-                ))}
+                  ))}
               </Flex>
               <div className='order-history__total'>
                 <Flex vertical gap={'20px'}>
@@ -118,9 +135,16 @@ export const ProfileOrderHistory = () => {
               <Button
                 size='large'
                 iconPosition='end'
-                icon={<ArrowUpOutlined style={{ color: '#054EDB' }} />}
+                icon={
+                  itemsToShow.includes(order.id) ? (
+                    <ArrowUpOutlined style={{ color: '#054EDB' }} />
+                  ) : (
+                    <ArrowDownOutlined style={{ color: '#054EDB' }} />
+                  )
+                }
+                onClick={() => onShowAllClick(order.id)}
               >
-                Свернуть
+                {itemsToShow.includes(order.id) ? 'Свернуть' : 'Подробнее'}
               </Button>
             </Flex>
           </Flex>
