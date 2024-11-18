516c5ca689ba0c733b8a1d4f6cf8f476760c960c
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-11-18 16:02:24 +0600

    Rename pages/home to pages/Home because github capitalizes home

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -1,4 +1,4 @@
-import { Home } from 'pages/home';
+import { Home } from 'pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import { Catalog } from 'pages/catalog';
 import { ProductInfo } from 'pages/product-info';
